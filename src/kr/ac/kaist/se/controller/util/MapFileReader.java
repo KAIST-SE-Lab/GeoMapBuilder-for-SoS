eeff8e9b9717edd242a15d4db9933b1943cfef60
Young-Min Baek <ymbaek@se.kaist.ac.kr>
2021-01-12 10:05:28 +0900

    MapFileReader updated - JavaDoc updated.

--- src/kr/ac/kaist/se/controller/util/MapFileReader.java
+++ src/kr/ac/kaist/se/controller/util/MapFileReader.java
@@ -5,8 +5,17 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
+/**
+ * A class to read a map file
+ * @author ymbaek
+ */
 public class MapFileReader {
 
+    /**
+     * A method to read a map file
+     * @param fileName  Name(dir and file name) of a file
+     * @return String-based contents of the file read
+     */
     public String readMapFile(String fileName){
 
         String mapInitString = "";
